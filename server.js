54e0ebc3d4c19f7869efe04353d2a517f49e1fe0
Reed Barger <39719154+reedbarger@users.noreply.github.com>
2018-06-06 22:53:07 -0400

    Update .gitignore

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+})
